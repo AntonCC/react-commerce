610073d0f63d4c5d60dd9df566b1a459cd97fb1b
Anton Chetvertkov <antonchetvertkov@Antons-MacBook-Pro.local>
2020-02-03 14:33:08 -0500

    added redux

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -56,13 +56,13 @@ class SignUp extends React.Component {
                 <h2 className='title'>I do not have an account</h2>
                 <span>Sign up with your email and password</span>
                 <form className='sign-up-form' onSubmit={this.handleSubmit}>
-                    <FormInput type='text' name='displayName' value={displayName} onChange={this.handleSubmit} label='Display Name' required>
+                    <FormInput type='text' name='displayName' value={displayName} onChange={this.handleChange} label='Display Name' required>
                     </FormInput>
-                    <FormInput type='email' name='email' value={email} onChange={this.handleSubmit} label='Email' required>
+                    <FormInput type='email' name='email' value={email} onChange={this.handleChange} label='Email' required>
                     </FormInput>
-                    <FormInput type='password' name='password' value={password} onChange={this.handleSubmit} label='Password' required>
+                    <FormInput type='password' name='password' value={password} onChange={this.handleChange} label='Password' required>
                     </FormInput>
-                    <FormInput type='password' name='confirmPassword' value={confirmPassword} onChange={this.handleSubmit} label='Confirm Password' required>
+                    <FormInput type='password' name='confirmPassword' value={confirmPassword} onChange={this.handleChange} label='Confirm Password' required>
                     </FormInput>
                     <CustomButton type='submit'>SIGN UP</CustomButton>
                 </form>
