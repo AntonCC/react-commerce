c7912ea66a2e3f0f90c1539bde0269d0e9049cc0
Anton Chetvertkov <antonchetvertkov@Antons-MacBook-Pro.local>
2020-02-07 01:57:49 -0500

    implemented checkout, set up structured selectors for all mapStateToProps

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,9 @@ import { connect } from 'react-redux'
 import { auth } from '../../firebase/firebase.utils'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
+import { createStructuredSelector } from 'reselect'
+import { selectCurrentUser } from '../../redux/user/user.selector'
+import { selectCartHidden } from '../../redux/cart/cart.selector'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 
@@ -30,10 +33,10 @@ const Header = ({ currentUser, hidden }) => (
         }
     </div>
 )
-
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
-    currentUser,
-    hidden 
+// createStructuredSelector passes top level state to subsequent selectors
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
